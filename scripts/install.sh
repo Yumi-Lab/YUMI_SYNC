bc8a4f38d1b2e588a40378b0161786b12d773fb8
Stephan Wendel <me@stephanwe.de>
2024-03-12 20:03:20 +0100

    chore: wip
    Signed-off-by: Stephan Wendel <me@stephanwe.de>

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,6 +3,8 @@ set -e
 # Debug
 set -x
 
+PKGLIST="python3 python3-venv"
+
 main() {
     local rebuildvenv
     case "${@}" in
@@ -16,48 +18,15 @@ main() {
         rebuild_venv
         exit 0
     else
-        if ! get_python_version; then
-            install_python
-        fi
+        install_dependencies
         create_virtualenv
     fi
 }
 
-py_not_installed_msg() {
-    printf "ERROR: Python seem not to be installed on your System!\n"
-    py_install_hint
-}
-
-py3_not_installed_msg() {
-    printf "ERROR: Your Python version too old! At least version 3 is required!\n"
-    py_install_hint
-}
-
-py_install_hint() {
-    printf "Trying to install Python3'\n"
-}
-
-get_python_version() {
-    local major version py_bin
-    py_bin="$(which python3 2>/dev/null)"
-    if [[ -z "${py_bin}" ]]; then
-        py_not_installed_msg
-        return 1
-    else
-        version="$("${py_bin}" --version)"
-        major="$(cut -f2 -d" " <<< "${version}" | cut -f1 -d".")"
-        if [[ "${major}" -lt 3 ]]; then
-            py3_not_installed_msg
-            return 1
-        else
-            return 0
-        fi
-    fi
-}
-
-install_python() {
+install_dependencies() {
     sudo apt-get update --allow-releaseinfo-change
-    sudo apt-get install --yes python3
+    # shellcheck disable=SC2086
+    sudo apt-get install --yes ${PKGLIST#}
 }
 
 create_virtualenv() {
