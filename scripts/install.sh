b426db421137bd2442a7470788a7563b4cbf3944
Stephan Wendel <me@stephanwe.de>
2024-03-12 21:09:36 +0100

    chore: wip
    Signed-off-by: Stephan Wendel <me@stephanwe.de>

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,6 +5,11 @@ set -x
 
 PKGLIST="python3 python3-venv"
 
+SERVICE_FILE_PATH="/etc/systemd/system/yumi_sync.service"
+
+[[ -n $BASE_USER ]] || BASE_USER="$(whoami)"
+[[ "${BASE_USER}" = "root" ]] && BASE_USER="${SUDO_USER}"
+
 main() {
     local rebuildvenv
     case "${@}" in
@@ -16,10 +21,12 @@ main() {
     esac
     if [[ "${rebuildvenv}" = "true" ]]; then
         rebuild_venv
+        printf "Rebuilding virtual environment done!\n"
         exit 0
     else
         install_dependencies
         create_virtualenv
+        install_service
     fi
 }
 
@@ -48,6 +55,31 @@ rebuild_venv() {
         rm -rf "${PWD}/venv"
     fi
     create_virtualenv
+}
+
+create_service_file() {
+    cat > "${SERVICE_FILE_PATH}" << EOF
+[Unit]
+Description=Yumi Sync Service
+Requires=network-online.target
+After=network-online.target
+
+[Service]
+Type=simple
+ExecStart=${PWD}/venv/bin/python ${PWD}/yumi_sync/yumi_sync.py
+WorkingDirectory=/home/${BASE_USER}/YUMI_SYNC
+Restart=always
+User=${BASE_USER}
+
+[Install]
+WantedBy=multi-user.target
+
+EOF
+}
+
+install_service() {
+    printf "Install Yumi Sync service ..."
+    create_service_file
 }
 
 if [[ "${BASH_SOURCE[0]}" = "${0}" ]]; then
