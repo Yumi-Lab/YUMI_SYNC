fcfb19f21c16fa7ca2a588f2a22bbb8e17d34727
Stephan Wendel <me@stephanwe.de>
2024-03-15 21:39:46 +0100

    chore: wip
    Signed-off-by: Stephan Wendel <me@stephanwe.de>

--- scripts/install.sh
+++ scripts/install.sh
@@ -27,13 +27,15 @@ main() {
         install_dependencies
         create_virtualenv
         install_service
+        add_moonraker_update
+        generate_moonraker_asvc
     fi
 }
 
 install_dependencies() {
     apt-get update --allow-releaseinfo-change
     # shellcheck disable=SC2086
-    apt-get install --yes ${PKGLIST#}
+    apt-get install --yes ${PKGLIST}
 }
 
 create_virtualenv() {
@@ -78,8 +80,54 @@ EOF
 }
 
 install_service() {
-    printf "Install Yumi Sync service ..."
+    printf "Install Yumi Sync service ...\n"
     create_service_file
+    printf "Enable Yumi Sync Service ...\n"
+    systemctl enable "$(basename "${SERVICE_FILE_PATH}")"
+    if [[ ! -d  /proc ]]; then
+        printf "Start Yumi Sync Service ...\n"
+        systemctl start "$(basename "${SERVICE_FILE_PATH}")"
+    fi
+}
+
+add_moonraker_update() {
+
+    if [[ -d "/home/${BASE_USER}/printer_data/config" ]]; then
+    cat > "/home/${BASE_USER}/printer_data/config/yumi_sync.cfg" <<EOF
+[update_manager yumi_sync]
+type: git_repo
+path: ~/YUMI_SYNC
+origin: https://github.com/Yumi-Lab/YUMI-SYNC.git
+primary_branch: main
+managed_services: yumi_sync
+install_script: $INSTALL_SCRIPT_PATH
+EOF
+    chown "${BASE_USER}":"${BASE_USER}" "/home/${BASE_USER}/printer_data/config/yumi_sync.cfg"
+    else
+        printf "WARN: could not install update entry...\n"
+        return
+    fi
+
+    if [[ -f "/home/${BASE_USER}/printer_data/config/moonraker.conf" ]]; then
+        echo "[include yumi_sync.cfg]" >> "/home/${BASE_USER}/printer_data/config/moonraker.conf"
+    fi
+}
+
+generate_moonraker_asvc() {
+    cat > "/home/${BASE_USER}/printer_data/moonraker.asvc" << EOF
+klipper_mcu
+webcamd
+MoonCord
+KlipperScreen
+moonraker-telegram-bot
+moonraker-obico
+sonar
+crowsnest
+octoeverywhere
+ratos-configurator
+yumi_sync
+EOF
+    chown "${BASE_USER}":"${BASE_USER}" "/home/${BASE_USER}/printer_data/moonraker.asvc"
 }
 
 if [[ "${BASH_SOURCE[0]}" = "${0}" ]]; then
--- scripts/uninstaller.sh
+++ scripts/uninstaller.sh
@@ -1,63 +1,26 @@
 #!/bin/bash
 
-# Define paths for the script, the systemd service file, and the moonraker.conf file
-SCRIPT_PATH="/home/pi/YUMI_SYNC/yumi_sync.py"
 SERVICE_PATH="/etc/systemd/system/yumi_sync.service"
-REPO_DIR="/home/pi/YUMI_SYNC"
-MOONRAKER_CONF="/home/pi/printer_data/config/moonraker.conf"
-INSTALL_SCRIPT_PATH="/home/pi/YUMI_SYNC/install.sh"
-CONFIG_SYNC_GIT='[update_manager Yumi_Sync]
-type: git_repo
-path: ~/YUMI_SYNC
-origin: https://github.com/Yumi-Lab/YUMI_SYNC.git
-primary_branch: main
-managed_services: yumi_sync
-install_script: $INSTALL_SCRIPT_PATH'
+MOONRAKER_CONF="${HOME}/printer_data/config/moonraker.conf"
 
 # Stop the service if it is running
-systemctl stop yumi_sync.service
+sudo systemctl stop yumi_sync.service
 
 # Disable the service to prevent it from starting on boot
-systemctl disable yumi_sync.service
+sudo systemctl disable yumi_sync.service
 
 # Remove the service file from systemd
 rm -f "$SERVICE_PATH"
 
 # Remove the YUMI_SYNC repository directory
-if [ -d "$REPO_DIR" ]; then
-    rm -rf "$REPO_DIR"
+if [ -d "${HOME}/YUMI_SYNC" ]; then
+    rm -rfv "${HOME}/YUMI_SYNC"
 fi
 
 # Remove the moonraker.conf config-sync-git if it exists
-sed -i '/$CONFIG_SYNC_GIT/d' "$MOONRAKER_CONF"
+sed -i '/\[include yumi_sync.cfg\]/d' "$MOONRAKER_CONF"
 
 # Remove the monitoring_state.json file if it exists
-rm -f "/home/pi/monitoring_state.json"
-
-# Optionally, uninstall dependencies (requests and netifaces)
-
-# Uninstall requests if it was installed
-if python3 -c "import requests" &>/dev/null; then
-    echo "Uninstalling requests..."
-    sudo pip3 uninstall -y requests
-fi
-
-# Uninstall netifaces if it was installed
-if python3 -c "import netifaces" &>/dev/null; then
-    echo "Uninstalling netifaces..."
-    sudo apt remove -y python3-netifaces
-fi
-
-# Check if python3 was installed only for YUMI_SYNC
-if ! dpkg -l python3 | grep -q '^.i'; then
-    echo "Uninstalling python3..."
-    sudo apt remove -y python3
-fi
-
-# Check if pip3 was installed only for YUMI_SYNC
-if ! dpkg -l python3-pip | grep -q '^.i'; then
-    echo "Uninstalling pip3..."
-    sudo apt remove -y python3-pip
-fi
+rm -fv "${HOME}/monitoring_state.json"
 
 echo "Uninstallation completed."
